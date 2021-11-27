1697daf3990a36e91e49b0d679c0ca53b3a37677
gourav <gvirus21yt@gmail.com>
2021-11-27 16:37:58 +0530

    added basic navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import Button from "../styled-items/Button";
+import logo from "../../img/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="nav__elephant--container">
+      <div className="nav__horse--container">
+        <div className="logo__container">
+          <div className="logo">
+            <img src={logo} alt="gv-logo" />
+          </div>
+        </div>
+        <ul className="navlinks__container">
+          <li>
+            <a className="navlink" href="">
+              Home
+            </a>
+          </li>
+          <li className="navlinks__separator--dot">.</li>
+          <li>
+            <a className="navlink" href="">
+              About
+            </a>
+          </li>
+          <li className="navlinks__separator--dot">.</li>
+          <li>
+            <a className="navlink" href="">
+              Projects
+            </a>
+          </li>
+        </ul>
+
+        <Button name="contact me" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/styled-items/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import
+
+const Button = ({ name }) => {
+  return (
+    <div className="button">
+      <button class="btn-hover color">{name}</button>
+    </div>
+  );
+};
+
+export default Button;
