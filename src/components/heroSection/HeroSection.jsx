9a1ca7ca9d2fba4ca957229ac3cc68b1fb761bdd
gourav <gvirus21yt@gmail.com>
2021-11-29 23:11:39 +0530

    updated NeueMontreal font for website and fixed some minor issues

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -5,7 +5,8 @@ const HeroSection = () => {
   return (
     <div className="herosection__elephant--container">
       <div className="herosection__info--container">
-        <h1>Hi i am gourav kumar</h1>
+        <h1>Hi</h1>
+        <h1>I am Gourav kumar</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
           esse soluta libero, laboriosam officia minima et harum dolorum nemo
