50d3a50462a86ddd1a3c7c88dadee1ade1ea825e
gourav <gvirus21yt@gmail.com>
2021-12-05 11:21:56 +0530

    herosection redesigned

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import graphic from "../img/undraw-1.svg";
 
 const HeroSection = () => {
   return (
     <div className="herosection__elephant--container">
+      <div className="herosection__greet--container">
+        <h2>
+          Hi, <span>I am</span>
+        </h2>
+      </div>
       <div className="herosection__info--container">
-        <h2>Hi, I am</h2>
-        <h1>Gourav kumar</h1>
+        <div className="herosection__info--container-upper">
+          <h1>Gourav</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="herosection__info--container-lower">
+          <h1>Kumar</h1>
+          <a href="">Know more </a>
+        </div>
+      </div>
+      <div className="herosection__graphic">
+        <img className="herosection__graphic-art" src={graphic} alt="" />
       </div>
     </div>
   );
